20658b986c89f9c172a75fb43209d2928ae7235c
WEB3 <WEB3@CRPBeauvoir.fr>
2020-02-06 16:42:39 +0100

    The last commit

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
+
+//importer mes routes
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
 //connection to mongoDB
 mongoose.connect('mongodb+srv://Sura:Sur@2510@cluster0-ejcqz.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -18,33 +24,9 @@ app.use((req, res, next) => {
 
   app.use(bodyParser.json());
 
-  app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message:'Objet crée !'
-    });
-  });
+  app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+  app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes);
 
 module.exports = app;
